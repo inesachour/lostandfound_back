d262d8754bdff492aa6a25c3f304ebe49f22442c
safa-abidi <safa.abidi99@gmail.com>
2022-04-06 15:18:02 +0100

    fix model types

--- src/publications/publications.model.ts
+++ src/publications/publications.model.ts
@@ -5,9 +5,34 @@ export const PublicationSchema = new mongoose.Schema({
   description: { type: String, required: true },
   date: { type: String, required: true },
   tempsCreation: { type: String, required: true },
-  location: { type: String, required: false },
-  images: { type: String, required: false },
-  owner: { type: String, required: true },
+  location: {
+    coordinates: { type: ['Double'], required: true },
+    type: { type: 'String', required: true },
+    //required: true, //CHANGED
+  },
+  images: { type: ['Mixed'], required: true },
+  owner: {
+    firstName: {
+      type: 'String',
+      required: true,
+    },
+    lastName: {
+      type: 'String',
+      required: true,
+    },
+    phone: {
+      type: 'String',
+      required: true,
+    },
+    email: {
+      type: 'String',
+      required: true,
+    },
+    photo: {
+      type: 'String',
+      required: false,
+    },
+  },
   category: { type: String, required: true },
   type: { type: String, required: true },
   status: { type: String, required: true },
