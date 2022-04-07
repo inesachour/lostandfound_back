81d12b26b976001aab0fd9a06d217ea0f8b45a90
unknown <inesachour@insat.u-carthage.tn>
2022-04-07 15:16:47 +0200

    conflicts fixing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-<<<<<<< HEAD
 import { UsersModule } from './users/users.module';
-=======
 import { PublicationsModule } from './publications/publications.module';
->>>>>>> d262d8754bdff492aa6a25c3f304ebe49f22442c
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://admin:v2U1XUy9DOg0SDOq@cluster0.7focf.mongodb.net/LostAndFound?retryWrites=true&w=majority',
     ),
-<<<<<<< HEAD
     UsersModule,
-=======
     PublicationsModule,
->>>>>>> d262d8754bdff492aa6a25c3f304ebe49f22442c
   ],
   controllers: [AppController],
   providers: [AppService],
